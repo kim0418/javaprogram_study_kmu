f385225ec1f1ad07da887edeeace47ec2fbb88cf
kim0418 <59432564+kim0418@users.noreply.github.com>
2024-01-03 17:06:02 +0900

    24.01.03 Javaprogramming 실습

--- 24.01.03실습/ScoreArray.java
+++ 24.01.03실습/ScoreArray.java
@@ -1,25 +1,33 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ScoreArray {
     public static void main(String[] args){
         String course[] = {"Java","C++","HTML5","컴퓨터구조","안드로이드"};
         int score[] = {95,88,76,62,55};
+        Scanner scanner =new Scanner(System.in);
 
         System.out.println("want exit, press 'exit'");
         while(true){
-            Scanner scanner =new Scanner(System.in);
             System.out.print("과목이름 >>> ");
             String class_name = scanner.nextLine();
             if(class_name.equals("exit"))
                 break;
-            for(int i=0;i<course.length;i++){
-                if(class_name.equals(course[i])){
-                    System.out.println(class_name+"의 점수는 "+score[i]);
-                }
-                if(i==course.length-1 && !class_name.equals(course[i])){
-                    System.out.println("없는 과목입니다.");
-                }
+            if(searcher(course,class_name)==-1){
+                System.out.println("없는 과목 입니다.");
+            }
+            else{
+                System.out.println(class_name + "의 점수는 " + score[searcher(course,class_name)]);
             }
         }
+        scanner.close();
+    }
+    public static int searcher(String[] arr,String target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].equals(target)){
+                return i;
+            }
+        }
+        return -1;
     }
 }
